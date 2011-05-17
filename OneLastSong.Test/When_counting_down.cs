b5149cb97895f9a9717938ba748d1eb597d39c3b
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-05-17 10:02:59 +0800

    more

---
+++ OneLastSong.Test/When_counting_down.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace OneLastSong.Test
+{
+    [TestFixture]
+    [Category("Countdown")]
+    public class When_counting_down
+    {
+        private readonly Countdown countdown = Countdown.FromMinutes(1);
+        private readonly ManualResetEvent expired = new ManualResetEvent(false);
+        private int ticks;
+
+        public When_counting_down()
+        {
+            countdown.Tick += () => ticks++;
+            countdown.Expired += () => expired.Set();
+            countdown.Start();
+            
+            expired.WaitOne(TimeSpan.FromSeconds(5));
+        }
+
+        [Test]
+        public void It_should_tick_five_times()
+        {
+            Assert.That(ticks, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void It_should_show_fifty_five_seconds_remaining()
+        {
+            Assert.That(countdown.TimeRemaining, Is.EqualTo(TimeSpan.FromSeconds(55)));
+        }
+    }
+}
